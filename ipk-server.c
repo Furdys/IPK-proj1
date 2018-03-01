ab06086fe5f5c408dcf3ac8a96fe762826a7a578
Jiří Furda <Furdys@users.noreply.github.com>
2018-03-01 01:06:01 +0100

    Reading etc/passwd

--- ipk-server.c
+++ ipk-server.c
@@ -17,6 +17,7 @@
 // --- Prototypes ---
 void getArguments(int argc, char** argv, int* port);
 void errorExit(char* msg);
+void getUserInfo(char flag, char* login);
 
 
 // --- Functions ---
@@ -87,7 +88,101 @@ int main(int argc, char** argv)
 	if(n < 0)
 		errorExit("Couldn't write socket");
 	
+	
+	
+	
+	///////////////////////////////////
+
+/*
+	getUserInfo('n', "furdys");
+	getUserInfo('h', "furdys");
+	getUserInfo('l', "sys");
+	getUserInfo('l', NULL);
+*/
+
 	return 0;
+}
+
+void getUserInfo(char flag, char* login)
+{
+	FILE *file = fopen("/etc/passwd", "r");
+	if(file == NULL)
+		errorExit("Couldn't open file");	
+	
+	int found = 0;	// Found result
+	char line[1024];	// Buffer for reading file
+	
+	if(flag == 'l')
+	{
+		if(login == NULL)
+		{
+			// -- Browsing file --	
+			while(fgets(line, sizeof(line), file) != NULL)
+			{	
+				char *token = strtok(line, ":");	// Get the first colon
+				printf("%s\n", token);
+			}
+		}
+		else
+		{
+			// -- Browsing file --	
+			while(fgets(line, sizeof(line), file) != NULL)
+			{	
+				if(!strncmp(login, line, strlen(login)))	// Matches the rule
+				{
+					char *token = strtok(line, ":");	// Get the first colon
+					printf("%s\n", token);
+				}
+			}			
+		}
+	}
+	else
+	{
+		// -- Resolving flag --
+		int skip; // How many colons should be skipped
+		if(flag == 'n')
+			skip = 4;
+		else if(flag == 'h')
+			skip = 5;
+		else
+		{
+			fclose(file);
+			errorExit("Invalid flag");
+		}
+		
+		
+		// -- Preparing login variable --
+		char newLogin[strlen(login)+2];
+		char suffix[2] = ":";
+		strcpy(newLogin, login);
+		strcat(newLogin, suffix);
+		
+		
+		// -- Searching in file --	
+		char line[1024];
+		while(fgets(line, sizeof(line), file) != NULL)
+		{
+			if(!strncmp(newLogin, line, strlen(newLogin)))	// Found the specified login
+			{
+				found = 1;
+				
+				// - Skipping colons -
+				char *token = strtok(line, ":");
+				for(int i = 0; i != skip; i++)
+					token = strtok(NULL, ":");
+					
+				// - Print result -
+				printf( "%s\n", token );
+			}
+		}		
+	}
+
+	fclose(file);
+
+	if(found == 0)
+	{
+		// @todo Return not found and print in on client's STDERR
+	}
 }
 
 
